5a370480c6bed849abdfe11f53e0189de9c92f4a
Marco Colombo <mcolombo>
2009-03-12 13:43:18 +0000

    Fix condition to report an error message.

--- src/backend.cpp
+++ src/backend.cpp
@@ -503,7 +503,7 @@ process_model(AmplModel *model) /* should be called with model==root */
       }
     }
     cout << endl;
-    if (n_nocsobj+n_other>0){
+    if (n_nocsobj+n_novar+n_other>0){
       printf("\nAMPL: ampl returned output\n");
       cout << "AMPL: Model without constraints and objectives: " <<
         n_nocsobj << "\n";
