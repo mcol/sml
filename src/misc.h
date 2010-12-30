41bcdb20b182bc07ad69d5b415e705b18bcf8064
Marco Colombo <mcolombo>
2010-12-30 11:32:17 +0000

    Don't declare to_string() as inline.

--- src/misc.h
+++ src/misc.h
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
+
 #ifndef MISC_H
 #define MISC_H
 
@@ -22,14 +23,10 @@
 /* ---------------------------------------------------------------------------
 string to_string()
 ---------------------------------------------------------------------------- */
-/* bit below from 
-http://notfaq.wordpress.com/2006/08/30/c-convert-int-to-string/
-to convert any numeric type into a string 
-(surprised C++ cannot do this out-of-the-box)                                */
-
+/* http://notfaq.wordpress.com/2006/08/30/c-convert-int-to-string/ */
+/** Convert any numeric type into a string */
 template <class T>
-inline std::string to_string (const T& t)
-{
+std::string to_string(const T& t) {
   std::stringstream ss;
   ss << t;
   return ss.str();
