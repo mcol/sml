1f58e4d097850e0111e9a802011a5683a02b8d5a
Marco Colombo <mcolombo>
2011-01-18 11:35:27 +0000

    Remove unnecessary forward declaration and #include.

--- interfaces/oops/OOPSBlock.h
+++ interfaces/oops/OOPSBlock.h
@@ -14,10 +14,8 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
-class OOPSBlock;
 
 #include "ExpandedModelInterface.h"
-#include "AmplsolverCalls.h"
 
 /** @class OOPSBlock
  *  OOPSBlock is an object that directly corresponds to a node in the
@@ -47,4 +45,3 @@ class OOPSBlock {
    */
   OOPSBlock(ExpandedModelInterface *rowmod, ExpandedModelInterface *colmod);
 };
-
